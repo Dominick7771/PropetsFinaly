bc5ab98377497c795a9c3cb8b1fc05005bc52773
Golub Anton <jukonda23@gmail.com>
2022-04-11 12:30:16 +0300

    fix bug

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,23 +1,22 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import HomeHeader from "../components/home/home_header/HomeHeader";
 import MainHome from "../components/home/home_main/middle_block_content/content_home/MainHome";
 import {getAuth, onAuthStateChanged, updateProfile} from "firebase/auth";
-import {useDispatch, useSelector} from "react-redux";
+import {useSelector} from "react-redux";
 import {doc, getDoc, setDoc} from "firebase/firestore";
 import {db} from "../utils/firebase";
 
 const HomePage = () => {
 
     const auth = getAuth();
-    const dispatch = useDispatch()
-    const user = useSelector(state => state.registration)
+    const userReg = useSelector(state => state.registration)
     let userInfo = localStorage.getItem('user')
     let initial = JSON.parse(userInfo)
 
     const update = () => {
-        if (user.name !== null) {
+        if (userReg.name !== null) {
             updateProfile(auth.currentUser, {
-                displayName: user.name,
+                displayName: userReg.name,
                 photoURL: 'https://firebasestorage.googleapis.com/v0/b/propets-f5334.appspot.com/o/avatar%2F715417e4-4e05-42d4-9ded-ce4429c93739?alt=media&token=6d173635-a4fb-46fe-ba3d-6036ab68e79e'
             })
                 .then()
@@ -25,7 +24,9 @@ const HomePage = () => {
                     console.log(error)
                 });
             onAuthStateChanged(auth, (user) => {
-                dispatch({type: 'SET_INFO_USER', payload: {name: user.name, id: user.uid}})
+                user['displayName'] = userReg.name;
+                user['photoURL'] = 'https://firebasestorage.googleapis.com/v0/b/propets-f5334.appspot.com/o/avatar%2F715417e4-4e05-42d4-9ded-ce4429c93739?alt=media&token=6d173635-a4fb-46fe-ba3d-6036ab68e79e';
+                localStorage.setItem('user', JSON.stringify(user))
             })
         }
     }
